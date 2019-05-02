206885d387e214a8f335ac2337b4b9d405c25603
Samir Aryamane <samirarya@gmail.com>
2019-05-01 19:39:24 -0700

    Added more verbosity and cleaned up the code for assertion explanation.

--- Patterns_For_Cleaner_Python/Leveraging_Asserts.py
+++ Patterns_For_Cleaner_Python/Leveraging_Asserts.py
@@ -1,4 +1,12 @@
 class Assertions_Usecase():
+    """Assertions are meant to flag out things which are meant
+    to be impossible to happen within the codebase, these are unrecoverable
+    errors to flag internal test cases. Assertions are debugging aid.
+    Never use Assert statements for data validations. Make sure the
+    assertion statements fail, before you use them in the field.
+    These are not a mechanism for handling runtime errors.
+    Asserts can be globally disabled with the interpreter setting.
+    """
 
     def __init__(self, product):
         self.product = product
@@ -9,8 +17,15 @@ class Assertions_Usecase():
         assert 0 <= price <= self.product['price']
         return 'Revised price of {} is now: ${}'.format(prod_name, price)
 
+    def never_failing_asserts(self, discount):
+        price = int(self.product['price'] * (1.0 - discount))
+        prod_name = self.product['name']
+        assert(0 <= price <= self.product['price'], 'AssertionError')
+        return 'Revised price of {} is now: ${}'.format(prod_name, price)
+
 
 shoes = {'name': 'Fancy shoes', 'price': 14900}
 
 asst = Assertions_Usecase(shoes)
 print(asst.apply_discount(0.50))
+print(asst.never_failing_asserts(2.0))
