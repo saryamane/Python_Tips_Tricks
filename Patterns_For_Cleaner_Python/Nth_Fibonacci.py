37818363f39838043922c122dd6139cd2693975e
Samir Aryamane <samirarya@gmail.com>
2019-08-06 12:24:07 -0700

    Added more memoization and iterative solution to Fibo

--- Patterns_For_Cleaner_Python/Nth_Fibonacci.py
+++ Patterns_For_Cleaner_Python/Nth_Fibonacci.py
@@ -20,7 +20,26 @@ class Solution:
 sol = Solution()
 print(sol.fib(6))
 
-# Do one with Memoization
+# Do one with Memoization, O(n) in both time and space complexity
+
+def getNthFibo(n, memoize = {1:0, 2:1}):
+    if n in memoize:
+        return memoize[n]
+    else:
+        memoize[n] = getNthFibo(n-1, memoize) + getNthFibo(n-2, memoize)
+        return memoize[n]
 
 
 # Do one with iterative approach, which O(1) complexity.
+
+# Time: O(n), space = O(1)
+
+def getNthFibo(n):
+    lastTwo = [0,1]
+    counter = 3
+    while counter < n:
+        nextFib = lastTwo[0] + lastTwo[1]
+        lastTwo[0] = lastTwo[1]
+        lastTwo[1] = nextFib
+        counter += 1
+    return lastTwo[1] if n > 1 else lastTwo[0]
