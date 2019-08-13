53a210263f3206e0eb26b921fb47ab65b12a50fb
Samir Aryamane <samirarya@gmail.com>
2019-08-12 17:39:17 -0700

    Added 3 number sum problem

--- Medium_Complexity/Three_Number_Sum.py
+++ Medium_Complexity/Three_Number_Sum.py
@@ -1,2 +1,24 @@
 def threeNumberSum(array, targetSum):
-    pass
+    array.sort()
+    triplets = []
+
+    for i in range(len(array) - 2):
+
+        first_idx = i + 1
+        second_idx = len(array) - 1
+
+        while first_idx < second_idx:
+            currentSum = array[i] + array[first_idx] + array[second_idx]
+            if currentSum == targetSum:
+                triplets.append([array[i], array[first_idx], array[second_idx]])
+                first_idx += 1
+                second_idx -= 1
+            elif currentSum < targetSum:
+                first_idx += 1
+            elif currentSum > targetSum:
+                second_idx -= 1
+    return triplets
+
+# Time complexity: O(N^2)
+
+# Space complexity: O(N)
