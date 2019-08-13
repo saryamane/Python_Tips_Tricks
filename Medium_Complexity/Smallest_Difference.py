a7b9cd963e24804369bbe40c9c482a3d0a519e17
Samir Aryamane <samirarya@gmail.com>
2019-08-13 10:22:49 -0700

    Added the correct solution for smallest difference problem

--- Medium_Complexity/Smallest_Difference.py
+++ Medium_Complexity/Smallest_Difference.py
@@ -1,2 +1,25 @@
 def smallestDifference(arrayOne, arrayTwo):
-    pass
+    arrayOne.sort()
+    arrayTwo.sort()
+    smallest = float("inf")
+    currentSum = float("inf")
+    smallest_array = []
+    firstIdx = 0
+    secondIdx = 0
+
+    while firstIdx < len(arrayOne) and secondIdx < len(arrayTwo):
+        firstNum = arrayOne[firstIdx]
+        secondNum = arrayTwo[secondIdx]
+        if firstNum < secondNum:
+            currentSum = secondNum - firstNum
+            firstIdx += 1
+        elif secondNum < firstNum:
+            currentSum = firstNum - secondNum
+            secondIdx += 1
+        else:
+            return [firstNum, secondNum]
+
+        if smallest > currentSum:
+            smallest = currentSum
+            smallest_array = [firstNum, secondNum]
+    return smallest_array
