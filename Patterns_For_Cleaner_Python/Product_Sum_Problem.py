864ab033816dffaa076ee1be46f15f313cef9f5e
Samir Aryamane <saryamane@salesforce.com>
2019-10-22 11:38:22 -0700

    Added the Product sum problem of easy complexity

---
+++ Patterns_For_Cleaner_Python/Product_Sum_Problem.py
@@ -0,0 +1,20 @@
+# Product Sum
+
+# Input is an array which can contain another arrays inside it.
+# Return the sum of the products within the passed array list.
+
+# Example: [x, [y,z]], output should be x + 2*(y + z)
+
+
+def product_sum(array, multiplier=1):
+    sum = 0
+    for element in array:
+        if type(element) is list:
+            sum += product_sum(element, multiplier+1)
+        else:
+            sum += element
+    return sum * multiplier
+
+
+output = product_sum([5,2,[7,-1], 3,[6,[-13,8], 4]])
+print(output)
