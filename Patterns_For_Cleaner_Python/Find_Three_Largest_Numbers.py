cbfd47b217ab36db448454a9a2dbba8e07b74f4e
Samir Aryamane <saryamane@salesforce.com>
2019-11-12 14:52:24 -0800

    Added practise to threeLargestSum

--- Patterns_For_Cleaner_Python/Find_Three_Largest_Numbers.py
+++ Patterns_For_Cleaner_Python/Find_Three_Largest_Numbers.py
@@ -5,7 +5,33 @@
 # Return: [10, 10, 12]
 
 
-def findThreeLargestNumbers(array):
+# def findThreeLargestNumbers(array):
+#     threeLargest = [None, None, None]
+#     for val in array:
+#         updateThreeLargest(threeLargest, val)
+#     return threeLargest
+
+
+# def updateThreeLargest(threeLargest, val):
+#     if threeLargest[2] is None or val > threeLargest[2]:
+#         handleUpdate(threeLargest, val, 2)
+#     elif threeLargest[1] is None or val > threeLargest[1]:
+#         handleUpdate(threeLargest, val, 1)
+#     elif threeLargest[0] is None or val > threeLargest[0]:
+#         handleUpdate(threeLargest, val, 0)
+
+
+# def handleUpdate(threeLargest, val, idx):
+#     for i in range(idx + 1):
+#         if idx == i:
+#             threeLargest[idx] = val
+#         else:
+#             threeLargest[i] = threeLargest[i + 1]
+
+
+# New practise code:
+
+def findThreeLargest(array):
     threeLargest = [None, None, None]
     for val in array:
         updateThreeLargest(threeLargest, val)
@@ -23,7 +49,11 @@ def updateThreeLargest(threeLargest, val):
 
 def handleUpdate(threeLargest, val, idx):
     for i in range(idx + 1):
-        if idx == i:
+        if i == idx:
             threeLargest[idx] = val
         else:
             threeLargest[i] = threeLargest[i + 1]
+
+# Try out the code:
+
+print(findThreeLargest([10, 5, 9, 10, 12]))
--- Patterns_For_Cleaner_Python/Product_Sum_Problem.py
+++ Patterns_For_Cleaner_Python/Product_Sum_Problem.py
@@ -16,5 +16,15 @@ def product_sum(array, multiplier=1):
     return sum * multiplier
 
 
+# def product_sum(array, multiplier = 1):
+#     sum = 0
+#     for element in array:
+#         if type(element) is list:
+#             sum += product_sum(element, multiplier + 1)
+#         else:
+#             sum += element
+#     return sum * multiplier
+
+
 output = product_sum([5,2,[7,-1], 3,[6,[-13,8], 4]])
 print(output)
