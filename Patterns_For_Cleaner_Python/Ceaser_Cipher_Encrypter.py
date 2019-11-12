c56e4ee9ffa928b77c9e59c1ebc13a718cbe8247
Samir Aryamane <saryamane@salesforce.com>
2019-11-12 15:07:49 -0800

    Added the practise for Ceaser Encrypter

--- Patterns_For_Cleaner_Python/Ceaser_Cipher_Encrypter.py
+++ Patterns_For_Cleaner_Python/Ceaser_Cipher_Encrypter.py
@@ -4,13 +4,29 @@
 # str = "xyz", 2
 # returns: "zab"
 
-def caesarCipherEncryptor(string, key):
+# def caesarCipherEncryptor(string, key):
+#     my_string = list(string)
+#     newKey = key % 26
+#     result = []
+#     for s in my_string:
+#         if (ord(s) + newKey) > 122:
+#             result.append(chr((96 + ((ord(s) + newKey) % 122))))
+#         else:
+#             result.append(chr(ord(s) + newKey))
+#     return "".join(result)
+
+
+def ceaserCipherEncrypter(string, key):
     my_string = list(string)
-    newKey = key % 26
+    new_key = key % 26
     result = []
+
     for s in my_string:
-        if (ord(s) + newKey) > 122:
-            result.append(chr((96 + ((ord(s) + newKey) % 122))))
+        if ord(s) + new_key > 122:
+            result.append(chr(96 + ((ord(s) + new_key) % 122)))
         else:
-            result.append(chr(ord(s) + newKey))
+            result.append(chr(ord(s) + new_key))
     return "".join(result)
+
+
+print(ceaserCipherEncrypter("xyz", 2))
