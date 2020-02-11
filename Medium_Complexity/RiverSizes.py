961ae19f4b5430d1fd9b088e93097d89f3eccfa9
Samir Aryamane <saryamane@salesforce.com>
2020-02-10 17:49:39 -0800

    Added riversizes problem

---
+++ Medium_Complexity/RiverSizes.py
@@ -0,0 +1,58 @@
+# Given an input matrix of different lengths of rows and columns,
+# the function should return the number of rivers in an array.
+# A river is identified by 1's in the matrix and 0 is considered as land.
+# Consecutive rivers can either be horiz or vertically, they cannot
+# be counted if seen diagonally.
+
+# I/P:
+
+# matrix = [1 0 0 0
+#           1 0 1 1
+#           0 0 1 0
+#           0 1 1 1]
+
+# O/p: [2, 6]
+
+
+def riverSizes(matrix):
+    sizes = []
+    visited = [[False for value in row] for row in matrix]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if visited[i][j]:
+                continue
+            traverseNode(i, j, visited, matrix, sizes)
+    return sizes
+
+
+def traverseNode(i, j, visited, matrix, sizes):
+    currentRiverSize = 0
+    nodesToExplore = [[i, j]]
+    while len(nodesToExplore):
+        currentNode = nodesToExplore.pop()
+        i = currentNode[0]
+        j = currentNode[1]
+        if visited[i][j]:
+            continue
+        visited[i][j] = True
+        if matrix[i][j] == 0:
+            continue
+        currentRiverSize += 1
+        unvisitedNeighbors = getUnvisitedNeighbor(i, j, matrix, visited)
+        for neighbor in unvisitedNeighbors:
+            nodesToExplore.append(neighbor)
+    if currentRiverSize > 0:
+        sizes.append(currentRiverSize)
+
+
+def getUnvisitedNeighbor(i, j, matrix, visited):
+    unvisitedNeighbors = []
+    if i > 0 and not visited[i - 1][j]:
+        unvisitedNeighbors.append([i - 1, j])
+    if i < len(matrix) - 1 and not visited[i + 1][j]:
+        unvisitedNeighbors.append([i + 1, j])
+    if j > 0 and not visited[i][j - 1]:
+        unvisitedNeighbors.append([i, j - 1])
+    if j < len(matrix[0]) - 1 and not visited[i][j + 1]:
+        unvisitedNeighbors.append([i, j + 1])
+    return unvisitedNeighbors
